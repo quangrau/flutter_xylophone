655a6a467355614d9d2e64a4c88a056b12319787
Quang Rau <lethanhquang910@gmail.com>
2020-12-21 01:19:22 +0800

    Init project

---
+++ android/app/src/main/kotlin/com/quangrau/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quangrau.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Xylophone Flutter App',
+      home: Xylophone(),
+    );
+  }
+}
+
+class Xylophone extends StatefulWidget {
+  @override
+  _XylophoneState createState() => _XylophoneState();
+}
+
+class _XylophoneState extends State<Xylophone> {
+  AudioCache _audio = AudioCache();
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isIOS) {
+      if (_audio.fixedPlayer != null) {
+        _audio.fixedPlayer.startHeadlessService();
+      }
+    }
+
+    // Play welcome sound
+    _audio.play('sweep.wav');
+  }
+
+  void playSound(int noteNumber) {
+    _audio.play('note$noteNumber.wav');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildKey(Colors.red, 'C', 1),
+            buildKey(Colors.orange, 'D', 2),
+            buildKey(Colors.yellow, 'E', 3),
+            buildKey(Colors.green, 'F', 4),
+            buildKey(Colors.teal, 'G', 5),
+            buildKey(Colors.blue, 'A', 6),
+            buildKey(Colors.purple, 'B', 7),
+            buildKey(Colors.pinkAccent, 'C', 8),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Expanded buildKey(Color color, String noteKey, int noteNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        child: Text(
+          noteKey,
+          style: TextStyle(color: Colors.white54, fontSize: 40.0),
+        ),
+        onPressed: () {
+          playSound(noteNumber);
+        },
+      ),
+    );
+  }
+}
